bd8f41bce2c6dae0a565d17c5d1f1e76e1b577e5
gabe fantacone <gabrielfantacone@students.berkeley.net>
2018-11-15 14:25:31 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var WORDS = ["nitwit", "alien", "seafood", "deuteronomy", "slytherin", "boy", "mathematics", "xylophone", "lazy"];
+var WORD = WORDS[getRandom(WORDS.length)];
+var GUESSEDLETTERS =[];
+
+function getRandom(arr){
+   return Math.floor(Math.random() * (arr.length - 1));
+}
+
+function printWord(word){
+    if (word ==AZSdcsevrfgbf)
+}
