fb2d7c670515e705ae0c84ef5c68ea3d68f8293a
gabe fantacone <gabrielfantacone@students.berkeley.net>
2018-11-29 14:24:29 -0800

    Figure out how to get the letters to appear in the underscore word. Also ask Albinson about requirements -- can we use a method besides indexOf??

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var WORDS = ["nitwit", "alien", "seafood", "deuteronomy", "slytherin", "boy", "m
 var WORD = WORDS[getRandom(WORDS)];
 var GUESSES = WORD.length;
 var GUESSEDLETTERS =[];
+var ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 console.log(WORD);
 
 function getRandom(arr){
@@ -9,6 +10,7 @@ function getRandom(arr){
 }
 
 function startGame(){
+    populate();
     var blanks = document.getElementById("blanks");
     for (var i = 0; i < WORD.length; i++){
         blanks.innerHTML += "_";
@@ -16,21 +18,69 @@ function startGame(){
 }
 
 function guessLetter(){
-    var letter = document.getElementById("currentLetterGuess").value.toString();
-    GUESSES--;
-    if (GUESSES > 0){
+    var letter = document.getElementById("currentLetterGuess").value;
+    if (GUESSES > 0 && !hasBeenGuessed(letter)){
         printWord(WORD, letter);
+        GUESSES--;
+    } else if (hasBeenGuessed(letter)){
+        alert("You have already guessed that letter. Please choose another one.");
+        // if letter has been guessed already, I don't take away a second guess
+        return null;
     } else {
-        alert("You have used up all of your guesses.");
+        alert("You have used up all of your guesses. Please reload the page and try again.");
+        return null;
     }
 }
 
 function printWord(word, letter){
+    // updates the underscore word
+    var newBlanks = document.getElementById("blanks").innerHTML;
     for (var i = 0; i < word.length; i++){
-        if (word[i] === letter){
-            document.getElementById("blanks").innerHTML[i] = letter;
+        if (word[i] === letter && newBlanks[i] === "_"){
+            // fills in the letter
+            newBlanks[i] = letter;
         }
     }
+    document.getElementById("blanks").innerHTML = newBlanks;
+    // assuming that the letter has not been guessed already, which is validated by hasBeenGuessed
     GUESSEDLETTERS.push(letter);
     document.getElementById("guessList").innerHTML = "Guessed letters: " + GUESSEDLETTERS.toString();
+    document.getElementById("guessesRemaining").innerHTML = "Guesses remaining: " + GUESSES;
+}
+
+// checks if the letter has been guessed already
+function hasBeenGuessed(letter){
+    for (var i = 0; i < GUESSEDLETTERS.length; i++){
+        if (GUESSEDLETTERS[i] === letter){
+            return true;
+        }
+    }
+    return false;
+}
+
+function inWord(word, letter){
+    for (var i = 0; i < word.length; i++){
+        if (word[i] === letter){
+            return true;
+        }
+    }
+    return false;
+}
+
+function populate(){
+    var container = document.getElementById("guessBox");
+    var select = document.createElement("select");
+    select.setAttribute("id", "currentLetterGuess");
+    for (var i = 0; i < 26; i++){
+        var option = document.createElement("option");
+        option.value = ALPHABET[i];
+        option.innerHTML = ALPHABET[i];
+        select.appendChild(option);
+    }
+    container.appendChild(select);
+    var button = document.createElement("button");
+    button.setAttribute("class", "w3-button w3-round w3-white w3-border w3-border-blue");
+    button.setAttribute("onclick", "guessLetter()");
+    button.innerHTML = "Go";
+    container.appendChild(button);
 }
